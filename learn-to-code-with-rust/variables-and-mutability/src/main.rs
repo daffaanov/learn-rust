3235fe6cb981c8d3b9a01aad73d4b01fb21b93b7
daffaanov <daffaanov@gmail.com>
2026-07-05 20:32:56 +0700

    variable and mutability done

--- learn-to-code-with-rust/variables-and-mutability/src/main.rs
+++ learn-to-code-with-rust/variables-and-mutability/src/main.rs
@@ -1,7 +1,20 @@
-fn main() {
-    let mut gym_reps = 10;
-    println!("I did {} reps at the gym today!", gym_reps);
+#[allow(unused_variables, unused_assignments, dead_code)]
+const TOUCHDOWN_POINTS: i32 = 6;
 
-    gym_reps = 15;
-    println!("I now plan to do {} reps", gym_reps);
+#[allow(unused_variables, unused_assignments)]
+fn main() {
+    let season = "Summer";
+    let mut points_scored = 28;
+
+    let event_time = "06:00";
+    let event_time = 6;
+    points_scored = 35;
+
+    println!(
+        "Points: {0}, Season: {1}, Event Time: {2}, Touchdown Points: {3}",
+        points_scored, season, event_time, TOUCHDOWN_POINTS
+    );
+
+    let favorite_beverage = "Coffee";
+    println!("Favorite Beverage: {favorite_beverage}");
 }
