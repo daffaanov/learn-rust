5fb23dfa2254fc833ebd8b389e52e3b8195f5041
daffaanov <daffaanov@gmail.com>
2026-07-05 19:47:24 +0700

    variables

--- learn-to-code-with-rust/variables-and-mutability/src/main.rs
+++ learn-to-code-with-rust/variables-and-mutability/src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+fn main() {
+    let mut gym_reps = 10;
+    println!("I did {} reps at the gym today!", gym_reps);
+
+    gym_reps = 15;
+    println!("I now plan to do {} reps", gym_reps);
+}
